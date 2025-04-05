95618094197b7dc136b36fdb7a0e47f33710c0bf
phihash <insharp0220@gmail.com>
2025-04-06 02:22:50 +0900

    feat 削除ボタンも追加

--- options/options.js
+++ options/options.js
@@ -1,24 +1,42 @@
 document.addEventListener("DOMContentLoaded",() => {
   const savedUrls = document.getElementById("savedUrls");
   chrome.storage.local.get(["shadowBookmarks"],(results) => {
-    const shadowBookmarks = results.shadowBookmarks || [];
+    let shadowBookmarks = results.shadowBookmarks || [];
     if(shadowBookmarks.length === 0){
       savedUrls.innerHTML = "<p>保存されたURLはありません</p>";
       return;
     }else{
+      const ul = document.createElement("ul");
       shadowBookmarks.forEach((bookmark) => {
         const url = bookmark.url;
         const title = bookmark.title;
-        const ul = document.createElement("ul");
         const li = document.createElement("li");
         const a = document.createElement("a");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "削除";
+        deleteButton.addEventListener("click",() => {
+          const confirmed = confirm("このURLを削除しますか");
+          if(confirmed){
+            chrome.storage.local.get(["shadowBookmarks"],(results) => {
+              let shadowBookmarks = results.shadowBookmarks || [];
+              shadowBookmarks = shadowBookmarks.filter((bookmark) => {
+                return bookmark.url !== url;
+              });
+              chrome.storage.local.set({shadowBookmarks},() => {
+                alert("削除しました");
+                savedUrls.removeChild(li);
+              });
+            });
+          }
+        });
         a.href = url;
         a.target = "_blank";
         a.textContent = title;
         li.appendChild(a);
+        li.appendChild(deleteButton);
         ul.appendChild(li);
-        savedUrls.appendChild(ul);
-      })
+      });
+      savedUrls.appendChild(ul);
     }
   });
 });
